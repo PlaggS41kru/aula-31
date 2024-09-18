6f3d40e288ea8096fa40c8d3c8a890fe56cc2599
PlaggS41kru <neris.gilson@hotmail.com>
2024-09-18 09:46:19 -0300

    Add files via upload

--- examples/objetos.js
+++ examples/objetos.js
@@ -26,3 +26,37 @@ const tv = {
 };
 
 tv.mudarAplicativo('Netflix');
+
+
+const computador = {
+  placaDeVideo: '',
+  polegadas: 30,
+  modelo: '',
+  memoria: '250gb',
+  sistemaOperacional: 'Windows',
+  aplicativos: ["Anti-virus", "VSCode", "Pycharm"],
+
+ ligar: () => console.log("O computador ligou."),
+ desligar: () => console.log("O computador desligou."),
+ reiniciar: () => console.log("O computador está sendo reiniciado."),
+
+  verAplicativos: function() {
+  this.aplicativos.forEach((app, index) => {
+    console.log(`${index + 1}. ${app}`);
+  });
+},
+
+  mudarAplicativo: function(aplicativo) {
+    if (this.aplicativos.includes(aplicativo)) {
+      console.log(`Mudando para o aplicativo: ${aplicativo}`);
+    } else {
+      console.log(`O aplicativo "${aplicativo} não está disponível.`);
+    }
+  },
+};
+
+computador.mudarAplicativo("VsCode");
+
+
+
+
